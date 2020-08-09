8bc8aec55a672a5845218acb011da5c9088ae155
marc lopes <marc_lopes2004@yahoo.fr>
2020-08-10 01:54:35 +0900

    fixed root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root :to 'basses/index'
+  root :to 'basses#index'
   resources :basses, only: [:index, :show, :new, :create] do
     resources :songs, only: [:new, :create]
     resources :comments, only: [:new, :create]
